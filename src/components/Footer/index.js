89fdd2109885a01eb5232f7f67894edc40d91af4
Rodrigo Gama <rodrigongama.adv@gmail.com>
2021-05-11 17:40:00 -0300

    improve: size, font-size and align

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,9 @@ import styles from './styles.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
+      <h2>JurisMilitar</h2>
+
       <div>
-        <h2>JurisMilitar</h2>
         <a
           href="https://www.instagram.com/juris.militar/"
           target="_blank "
@@ -15,16 +16,12 @@ export default function Footer() {
             <h3>Instagram</h3>
           </div>
         </a>
-      </div>
-      <div>
         <a href="mailto:juris.militar@gmail.com">
           <div className={styles.icons}>
             <img src="/icon-email.png" alt="Enviar e-mail" />
             <h3>Contate-nos por e-mail</h3>
           </div>
         </a>
-      </div>
-      <div>
         <a
           href="https://sacola.pagseguro.uol.com.br/1df72eaf-2fd7-4703-a30d-c8fe0fd36813"
           target="_blank"
